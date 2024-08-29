5b387a281a8709eb4bb8ca9b64b3d41eb2413071
abtahihasan <abtahihasan7472@gmail.com>
2024-08-29 22:25:37 +0600

    tutorial 3 done

--- index.ts
+++ index.ts
@@ -1,1 +1,39 @@
-let username: string = "abtahi hasan";
+// ** this is primitive types **
+// string
+let myName: string;
+
+myName = "abtahi hasan";
+
+// number
+let otp: number = 778854;
+
+otp = 6050;
+
+// boolean
+let isVerified: boolean;
+
+isVerified = true;
+
+// undefined
+const thisIsUndefined: undefined = undefined;
+// null
+const thisIsNull: null = null;
+
+// ** Non primitive types **
+
+// array
+const numbers: number[] = [];
+
+numbers.push(50);
+
+// tuple
+
+// const people: [number, string] = [540, "jon"];
+
+const people: (string | number)[] = [];
+
+people.push(2);
+people.push("fasdf");
+people.push("fasdf");
+people.push("fasdf");
+people.push(2);

e31c4d8dd02998c78d9bd8ea75be0786baab47ef
abtahihasan <abtahihasan7472@gmail.com>
2024-09-01 21:20:21 +0600

    object done

--- index.ts
+++ index.ts
@@ -1,39 +1,10 @@
-// ** this is primitive types **
-// string
-let myName: string;
-
-myName = "abtahi hasan";
-
-// number
-let otp: number = 778854;
-
-otp = 6050;
-
-// boolean
-let isVerified: boolean;
-
-isVerified = true;
-
-// undefined
-const thisIsUndefined: undefined = undefined;
-// null
-const thisIsNull: null = null;
-
-// ** Non primitive types **
-
-// array
-const numbers: number[] = [];
-
-numbers.push(50);
-
-// tuple
-
-// const people: [number, string] = [540, "jon"];
-
-const people: (string | number)[] = [];
-
-people.push(2);
-people.push("fasdf");
-people.push("fasdf");
-people.push("fasdf");
-people.push(2);
+const user: {
+  name: string;
+  bio?: string;
+  role: "admin";
+  isVerified: boolean;
+} = {
+  name: "Jhon deo",
+  role: "admin",
+  isVerified: true,
+};
